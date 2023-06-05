e98db0534881eae9483d5effd86626b725d88bc0
KrasniKot <6187@holbertonstudents.com>
2023-06-05 09:03:50 -0700

    return value fixed

--- python-data_structures/2-replace_in_list.py
+++ python-data_structures/2-replace_in_list.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def replace_in_list(my_list, idx, element):
     if my_list[idx]:
-        mylist[idx] = element
-    return mylist[idx]
+        my_list[idx] = element
+    return my_list
