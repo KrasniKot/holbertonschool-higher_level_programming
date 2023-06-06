4e7edfad5b87688f3b9cbe5916d4aa3e155e4515
KrasniKot <6187@holbertonstudents.com>
2023-06-06 15:47:02 -0700

    task 10 done

---
+++ python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,8 @@
+def divisible_by_2(my_list=[]):
+    tflist = []
+    for i in my_list:
+        if i % 2 == 0:
+            tflist.append(True)
+        else:
+            tflist.append(False)
+    return tflist
