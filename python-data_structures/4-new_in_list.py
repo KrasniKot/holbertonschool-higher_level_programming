1942062a8c5014bcbf84f34ade3ce2c26d43089c
KrasniKot <6187@holbertonstudents.com>
2023-06-06 08:14:58 -0700

    lines order changed

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
+    cpylist = my_list.copy()
     if idx >= 0 and idx < len(my_list):
-        cpylist = my_list.copy()
         cpylist[idx] = element
     return cpylist
