89e7e8a9cc186d1cae00c35b23a4b562102961a1
KrasniKot <6187@holbertonstudents.com>
2023-06-06 08:13:41 -0700

    if modified and return value

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
-    if idx >= 0 and my_list[idx]:
+    if idx >= 0 and idx < len(my_list):
         cpylist = my_list.copy()
         cpylist[idx] = element
-        return cpylist
-    return my_list
+    return cpylist
