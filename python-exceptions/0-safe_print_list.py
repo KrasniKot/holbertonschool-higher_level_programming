385f579cd808b350e9a161c87d81210c9ba1a0ac
KrasniKot <6187@holbertonstudents.com>
2023-06-09 10:23:45 -0700

    task 0 done

---
+++ python-exceptions/0-safe_print_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    pel = 0
+    for el in range(x):
+        try:
+            print("{:d}".format(my_list[el]), end="")
+            pel += 1
+        except:
+            break
+    print()
+    return pel
