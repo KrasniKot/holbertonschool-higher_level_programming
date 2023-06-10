672eb106eb01c4a526c020b5fc59ddaf091a10a9
KrasniKot <6187@holbertonstudents.com>
2023-06-09 20:27:40 -0700

    task 4 don

---
+++ python-exceptions/4-list_division.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+def list_division(my_list_1, my_list_2, list_length):
+    flist = []
+    for idx in range(list_length):
+        idxrlt = 0
+        try: 
+            idxrlt= my_list_1[idx] / my_list_2[idx]
+        except TypeError:
+            print("wrong type")
+        except ZeroDivisionError:
+            print("division by 0")
+        except IndexError:
+            print("out of range")
+        finally:
+            flist.append(idxrlt)
+    return flist
