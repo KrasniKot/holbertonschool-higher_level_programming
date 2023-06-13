6a13dd4c21b021c971b4b6c8f760b3cff4aa2759
KrasniKot <6187@holbertonstudents.com>
2023-06-13 11:13:07 -0700

    task 1 done

---
+++ python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Creates a class named Square"""
+
+
+class Square:
+    """DEfines a square"""
+    def __init__(self, size):
+    """
+    Initializes a Square
+    Atributtes:
+        size: Square size.
+    """
+    self.__size = size
