43c2410fc0ccdb558ddb2f46b46bf74557cc6e9c
KrasniKot <emanueltrias9@gmail.com>
2023-06-14 05:40:18 -0700

    5 if size == 0 added

--- python-classes/5-square.py
+++ python-classes/5-square.py
@@ -35,7 +35,11 @@ class Square:
         self.__size = value
 
     def my_print(self):
-        for i in range(self.__size):
-            for j in range(self.__size):
-                print("#", end="")
+        """prints a square using '#'"""
+        if self.__size == 0:
             print()
+        else:
+            for i in range(self.__size):
+                for j in range(self.__size):
+                    print("#", end="")
+                print()
