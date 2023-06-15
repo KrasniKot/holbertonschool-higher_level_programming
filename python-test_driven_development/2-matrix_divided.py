502eb25a85ffc39f5ea281f1f0ea758be751d2a7
KrasniKot <6187@holbertonstudents.com>
2023-06-15 15:43:17 -0700

    div != div added

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -8,20 +8,20 @@ def matrix_divided(matrix, div):
     """
     Divides each element of a matrix and returns the result
     """
+    error = "matrix must be a matrix (list of lists) of integers/floats"
+
     if type(matrix) != list:
-        raise TypeError("matrix must be a matrix "\
-                "(list of lists) of integers/floats")
+        raise TypeError(error)
 
     for elist in matrix:
         if any(type(el) != int and type(el) != float for el in elist)\
                 or len(elist) == 0:
-            raise TypeError("matrix must be a matrix "\
-                    "(list of lists) of integers/floats")
+            raise TypeError(error)
 
     if any(len(matrix[0]) != len(elist) for elist in matrix):
         raise TypeError("Each row of the matrix must have the same size")
 
-    if type(div) != int and type(div) != float:
+    if type(div) != int and type(div) != float or div != div:
         raise TypeError("div must be a number")
 
     if div == 0:
