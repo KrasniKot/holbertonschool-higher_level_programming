da66e9055299738beacac343f7483de81b0e9821
KrasniKot <6187@holbertonstudents.com>
2023-06-16 12:15:25 -0700

    task 5 done

---
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""Unittest for max_integer([..])
+"""
+import unittest
+max_integer = __import__('6-max_integer').max_integer
+
+class TestMaxInteger(unittest.TestCase):
+    """Testing max_integer()"""
+
+    def test_Result(self):
+        """Checks for the return value to be correct"""
+        self.assertEqual(max_integer([1, 4, 19, 0]), 19)
+        self.assertEqual(max_integer([-30, -1, -3]), -1)
+        self.assertEqual(max_integer([4.5, 1, 9.2, 9.1]), 9.2)
+        self.assertEqual(max_integer(["a", "b", "c"]), "c")
+        self.assertEqual(max_integer([20, float("Inf")]), float("inf"))
+
+    def test_CorrectType(self):
+        """Checks for a TypeError to be raised"""
+        self.assertRaises(max_integer(["five", 9, 17, 14]), TypeError)
+        self.assertRaises(max_integer([4, None, 19, 1]), TypeError)
+
+    def test_EmptyList(self):
+        """Checks for the return value to be correct (None)"""
+        self.assertEqual(max_integer([]), None)
