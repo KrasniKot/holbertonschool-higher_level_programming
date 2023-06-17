52636de8f55e5bd2a3e21593cd94134585f5f9af
KrasniKot <6187@holbertonstudents.com>
2023-06-16 23:23:56 -0700

    indentation 1 fixed

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -8,33 +8,33 @@ class Rectangle:
     def __init__(self, width=0, height=0):
         """Initialization of an instance"""
 
-        self.__width = width
-        self.__height = height
+        self.width = width
+        self.height = height
 
-        @property
-        def width(self):
-            """Getter method"""
-            return self.__width
+    @property
+    def width(self):
+        """Getter method"""
+        return self.__width
 
-        @width.setter
-        def width(self, value):
-            """Setter method"""
-            if type(value) != int:
-                raise TypeError("width must be an integer")
-            if value < 0:
-                raise ValueError("width must be >= 0")
-            self.__width = value
+    @width.setter
+    def width(self, value):
+        """Setter method"""
+        if type(value) != int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
 
-        @property
-        def height(self):
-            """Getter method"""
-            return self.__height
+    @property
+    def height(self):
+        """Getter method"""
+        return self.__height
 
-        @height.setter
-        def height(self, value):
-            """Setter method"""
-            if type(value) != int:
-                raise TypeError("height must be an integer")
-            if value < 0:
-                raise ValueError("height must be >= 0")
-            self.__height = vailue
+    @height.setter
+    def height(self, value):
+        """Setter method"""
+        if type(value) != int:
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
