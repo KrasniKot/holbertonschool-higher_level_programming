6ebeba2752b6ec5c8b0eca557b8178d63455d014
KrasniKot <6187@holbertonstudents.com>
2023-06-17 15:03:54 -0700

    task 8 isinstance() instead of type()

--- python-more_classes/8-rectangle.py
+++ python-more_classes/8-rectangle.py
@@ -75,8 +75,8 @@ class Rectangle:
     @staticmethod
     def bigger_or_equal(rect_1, rect_2):
         """Returns the bigger rectangle"""
-        if type(rect_1) != Rectangle:
+        if not isinstance(rect_1, Rectangle):
             raise TypeError("rect_1 must be an instance of Rectangle")
-        if type(rect_2) != Rectangle:
+        if not isinstance(rect_2, Rectangle):
             raise TypeError("rect_1 must be an instance of Rectangle")
         return rect_1 if rect_1.area() >= rect_2.area() else rect_2
