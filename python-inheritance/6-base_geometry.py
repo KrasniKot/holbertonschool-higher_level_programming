adc19a97177367a50ae114eb96d7ebad0660780b
KrasniKot <6187@holbertonstudents.com>
2023-06-22 00:24:14 -0700

    task 6 done

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Contains the class BaseGeometry"""
+
+
+class BaseGeometry:
+    """class Basegeometry definition"""
+
+    def area(self):
+        """rasies an exception exception"""
+        raise Exception("area() is not implemented")
