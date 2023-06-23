93644161b222df646d59aad0cc68ce25be55524a
KrasniKot <6187@holbertonstudents.com>
2023-06-22 20:40:19 -0700

    task 0 done

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -4,5 +4,5 @@
 
 def read_file(filename=""):
     """Reads a file and prints it to the stdout"""
-    with open("my_file_0.txt", encoding="utf-8") as file:
+    with open("my_file_0.txt", encoding="utf8") as file:
         print(file.read(), end="")
