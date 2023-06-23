1f5d64ed408a660f79f804ebb41134b8bc4ed610
KrasniKot <6187@holbertonstudents.com>
2023-06-23 16:42:16 -0700

    task 6 done

---
+++ python-input_output/6-load_from_json_file.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Contains load_from_json_file()"""
+import json
+
+def load_from_json_file(filename):
+    """Creates an object from a json file"""
+    with open(filename, "r", encoding="utf-8") as f:
+        return json.load(f)
