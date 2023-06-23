45865b7a6484268eceb333f0dbcbdce737a03e21
KrasniKot <6187@holbertonstudents.com>
2023-06-22 20:54:46 -0700

    0 idk

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -5,5 +5,5 @@
 def read_file(filename=""):
     """Reads a file and prints it to the stdout"""
     with open("my_file_0.txt", encoding=i"UTF8") as f:
-        for line in f:
+        for line in f.read():
             print(f"{line}", end="")
