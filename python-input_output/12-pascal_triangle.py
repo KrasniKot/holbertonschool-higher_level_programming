03b7d4e1c6a8cacce2eb590807cf4e5406242559
KrasniKot <6187@holbertonstudents.com>
2023-06-24 13:48:03 -0700

    12 done?

--- python-input_output/12-pascal_triangle.py
+++ python-input_output/12-pascal_triangle.py
@@ -4,8 +4,9 @@
 
 def pascal_triangle(n):
     """Returns a list of lists representing the Pascal's triangle"""
+    if n <= 0:
+        return [[]]
     tri = []
-    if n > 0:
-        for i in range(n):
-            tri += [list(str(11**i))]
+    for i in range(n):
+        tri += [list(str(11**i))]
     return tri
