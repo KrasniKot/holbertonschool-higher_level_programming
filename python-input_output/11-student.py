3145071f94ec566c986be72b0f6cf74acb152569
KrasniKot <6187@holbertonstudents.com>
2023-06-23 23:33:27 -0700

    task 10 documentation fixed

--- python-input_output/11-student.py
+++ python-input_output/11-student.py
@@ -5,7 +5,6 @@
 class Student:
     """Defines a Student"""
     def __init__(self, first_name, last_name, age):
-        """Initializes a Student"""
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
