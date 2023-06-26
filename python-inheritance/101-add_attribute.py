12f89a09ca2323e3d4646bf12eb22f8f5198ce7c
KrasniKot <6187@holbertonstudents.com>
2023-06-25 18:01:55 -0700

    task 101 done

--- python-inheritance/101-add_attribute.py
+++ python-inheritance/101-add_attribute.py
@@ -2,8 +2,8 @@
 """Contains add_attribute()"""
 
 
-def add_attribute(Obj, attr, val):
+def add_attribute(obj, attr, val):
     """Adds an attribute to an object if possible"""
     if not hasattr(obj, "__dict__"):
         raise TypeError("can't add new attribute")
-    setattr(obj, att, value)
+    setattr(obj, attr, val)
