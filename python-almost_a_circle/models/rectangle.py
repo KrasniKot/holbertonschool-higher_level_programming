386817c8dea57db58c76ebbe53081153340ea2ad
KrasniKot <emanueltrias9@gmail.com>
2023-06-27 09:35:13 -0700

    6

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -82,5 +82,4 @@ class Rectangle(Base):
 
     def __str__(self):
         """Override the str method"""
-        return f"[Rectangle] ({self.id}) {self.__x}/{self.__y}\
-- {self.__width}/{self.__height}"
+        return f"[Rectangle] ({self.id}) {self.__x}/{self.__y} - {self.__width}/{self.__height}"
