86023d551d729b2d5045c55494972cff72c1ac05
KrasniKot <emanueltrias9@gmail.com>
2023-06-27 08:18:46 -0700

    Error messages fixed 3

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -38,7 +38,7 @@ class Rectangle(Base):
         """width setter method"""
         if type(value) != int:
             raise TypeError("width must be an integer")
-        if value < 0:
+        if value <= 0:
             raise ValueError("width must be > 0")
         self.__width = value
 
@@ -47,7 +47,7 @@ class Rectangle(Base):
         """height setter method"""
         if type(value) != int:
             raise TypeError("height must be an integer")
-        if value < 0:
+        if value <= 0:
             raise ValueError("height must be > 0")
         self.__height = value
 
