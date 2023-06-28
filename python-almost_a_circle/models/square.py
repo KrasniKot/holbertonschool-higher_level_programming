b8400d4f667edb35a2fb76ada0eed50586705c46
KrasniKot <emanueltrias9@gmail.com>
2023-06-27 18:42:14 -0700

    idk ^2 19

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -10,7 +10,7 @@ class Square(Rectangle):
         super().__init__(size, size, x, y, id)
 
     def __str__(self):
-        """Overrides the inherited __str__ method"""
+        """Overrides the inherited the method"""
         return f"[Square] ({self.id}) {self.x}/{self.y} - {self.width}"
 
     @property
