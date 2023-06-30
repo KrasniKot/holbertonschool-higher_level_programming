e99dd9625145fbcafc80b8cbb37f76abb06f1bb1
KrasniKot <emanueltrias9@gmail.com>
2023-06-29 20:41:42 -0700

    task 17 done

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -41,3 +41,13 @@ class Base:
         if json_string is None or not json_string:
             return []
         return json.loads(json_string)
+
+    @classmethod
+    def create(cls, **dictionary):
+        """Returns an instance with all attributes already set"""
+        if cls.__name__ == "Rectangle":
+            inst = cls(1, 1)
+        else:
+            inst = cls(1)
+        inst.update(**dictionary)
+        return inst
