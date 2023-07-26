d095c531ea08f0b275fa767783a819bf11d7e507
KrasniKot <6187@holbertonstudents.com>
2023-07-26 12:12:35 -0700

    task 15 done

---
+++ SQL_more_queries/15-comedy_only.sql
@@ -0,0 +1,5 @@
+-- Lists all Comedy shows
+
+SELECT tv_shows.title FROM tv_shows, tv_show_genres, tv_genres
+WHERE tv_shows.id = tv_show_genres.show_id AND tv_show_genres.genre_id = tv_genres.id AND tv_genres.name = "Comedy"
+ORDER BY title ASC;
