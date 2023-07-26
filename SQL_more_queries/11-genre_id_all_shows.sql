dfaafeffafd7445956184c2abb7ce5bd7a0c60fc
KrasniKot <6187@holbertonstudents.com>
2023-07-26 09:47:15 -0700

    task 11 done

--- SQL_more_queries/11-genre_id_all_shows.sql
+++ SQL_more_queries/11-genre_id_all_shows.sql
@@ -1,3 +1,3 @@
 -- Lists all shows contained in hbtn_0d_tvshows that have at least one genre linked
 
-SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_shows, tv_show_generes WHERE tv_shows.id = tv_show_genere_id ORDER BY title, genere_id ASC;
+SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_shows, tv_show_genres WHERE tv_shows.id = tv_show_genres.show_id ORDER BY title, genre_id ASC;
