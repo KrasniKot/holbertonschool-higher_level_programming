f87acf02cf88c00bc3dee16972494b86392673a4
KrasniKot <6187@holbertonstudents.com>
2023-07-30 21:12:28 -0700

    task 9 idk g

--- python-object_relational_mapping/8-model_state_fetch_first.py
+++ python-object_relational_mapping/8-model_state_fetch_first.py
@@ -1,24 +1,27 @@
 #!/usr/bin/python3
 """
-Prints the first State object from the database hbtn_0e_6_usa
+Lists all State objects from the database hbtn_0e_6_usa
 """
+from sys import argv
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
 
 if __name__ == "__main__":
-    from sys import argv
-    from sqlalchemy import create_engine
-    from sqlalchemy.orm import sessionmaker
-    from model_state import State, Base
-
-    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}".format(argv[1],
-                           argv[2], argv[3]))
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(argv[1], argv[2], argv[3]),
+                           pool_pre_ping=True)
     Base.metadata.create_all(engine)
 
     Session = sessionmaker(bind=engine)
     session = Session()
-    data = session.query(State).first()
 
-    if data:
-        print("{}: {}".format(data.id, data.name))
+    fob = session.query(State).first()
+
+    if fob:
+        print("{}: {}".format(firts_object.id, firts_object.name))
     else:
         print("Nothing")
+
     session.close()
