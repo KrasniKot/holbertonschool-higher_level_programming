6a8c2dbbda802193e4cdc1a95f23a6f625df77a6
KrasniKot <6187@holbertonstudents.com>
2023-07-30 21:43:21 -0700

    task 9 done

--- python-object_relational_mapping/9-model_state_filter_a.py
+++ python-object_relational_mapping/9-model_state_filter_a.py
@@ -13,4 +13,15 @@ from sqlalchemy.orm import sessionmaker
 if __name__ == "__main__":
 
     engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}".format(
-        argv[1], argv[2], argv[3])
+        argv[1], argv[2], argv[3]))
+
+    Base.metadata.create_all(engine)
+
+    Session = sessionmaker(engine)
+    session = Session()
+
+    for object in session.query(State).filter(
+            State.name.like('%a%')).order_by(State.id):
+        print("{}: {}".format(object.id, object.name))
+
+    session.close()
