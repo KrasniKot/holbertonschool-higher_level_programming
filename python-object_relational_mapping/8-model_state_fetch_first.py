2b0374253ffb85cc21be7d113cba12d3bc31830f
KrasniKot <6187@holbertonstudents.com>
2023-07-30 19:57:33 -0700

    task 8

--- python-object_relational_mapping/8-model_state_fetch_first.py
+++ python-object_relational_mapping/8-model_state_fetch_first.py
@@ -13,8 +13,8 @@ if __name__ == "__main__":
                            argv[2], argv[3]))
 
     Base.metadata.create_all(engine)
-    session = Session()
     Session = sessionmaker(bind=engine)
+    session = Session()
 
     for object in session.query(State).order_by(State.id):
         print("{}: {}".format(object.id, object.name))
