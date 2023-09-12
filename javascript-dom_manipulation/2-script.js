3d723272d8ca4ebbc9e0166a45add34b96c9fe3c
KrasniKot <emanueltrias9@gmail.com>
2023-09-12 17:35:28 +0000

    task 2: tag selector fixed

--- javascript-dom_manipulation/2-script.js
+++ javascript-dom_manipulation/2-script.js
@@ -1,1 +1,1 @@
-document.querySelector("#red_header").onclick = function() {document.querySelector("heder").classList.add("red")};
+document.querySelector("#red_header").onclick = function() {document.querySelector("header").classList.add("red")};
